271097b311fe51df40c990ba3c8f6c0288f870c1
Alina Arap <alinaarap@gmail.com>
2021-01-27 14:25:39 +0200

    Initial commit

---
+++ public/js/chat.js
@@ -0,0 +1,24 @@
+const socket = io()
+
+socket.on('message', (mes) => {
+    console.log(mes);
+});
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    socket.emit('sendMessage', e.target.elements.message.value);
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.');
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation',
+            {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            });
+    });
+});
